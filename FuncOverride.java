0686785557c3e60f69bb6449f56ca374f690f275
shweta786 <shwetasrivastava1994@gmail.com>
2017-06-15 15:56:48 +0530

    Add files via upload

---
+++ FuncOverride.java
@@ -0,0 +1,8 @@
+//Used for dynamic binding i.e runtime polymorphism
+public class FuncOverride {
+
+		void run()
+		{
+			System.out.println("Vehicle is running");
+		}
+	}
---
+++ Override2.java
@@ -0,0 +1,11 @@
+
+public class Override2 {
+
+		void run(){System.out.println("Bike is running safely");}  
+		  
+		public static void main(String args[]){  
+		Override2 obj = new Override2();  
+		obj.run();  
+		}  
+	
+}
