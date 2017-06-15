d70d7590d298970de38045ce3aff64b4a276074c
shweta786 <shwetasrivastava1994@gmail.com>
2017-06-15 15:43:06 +0530

    Add files via upload

---
+++ PrivateK.java
@@ -0,0 +1,16 @@
+
+public class PrivateK {
+
+		private int data=40;  
+		private void msg(){System.out.println("Hello java");}  
+		}  
+		  
+		 class Simple{  
+		 public static void main(String args[]){  
+		   PrivateK obj=new PrivateK();  
+		//   System.out.println(obj.data);//Compile Time Error  
+		 //  obj.msg();//Compile Time Error  
+		   }  
+	
+}
+
